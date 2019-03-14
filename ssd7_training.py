1453e34552945331bcb3fe3b93f93a490ee5f11b
Athul Mathew Konoor <48464848+toshihiroryuu@users.noreply.github.com>
2019-03-14 15:26:09 +0530

    Create ssd7_training.py

---
+++ ssd7_training.py
@@ -0,0 +1,274 @@
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+from keras import backend as K
+from keras.models import load_model
+from math import ceil
+import numpy as np
+from matplotlib import pyplot as plt
+
+from keras_ssd7 import build_model
+from keras_ssd_loss import SSDLoss
+from keras_layer_AnchorBoxes import AnchorBoxes
+from keras_layer_L2Normalization import L2Normalization
+from ssd_box_encode_decode_utils import SSDBoxEncoder, decode_y, decode_y2
+from ssd_batch_generator import BatchGenerator
+
+img_height = 300 
+img_width = 480 
+img_channels = 3 
+subtract_mean = 127.5  
+divide_by_stddev = 127.5 
+n_classes=5
+scales = [0.08, 0.16, 0.32, 0.64, 0.96] 
+aspect_ratios = [0.5, 1.0, 2.0] 
+two_boxes_for_ar1 = True 
+steps = None 
+offsets = None 
+limit_boxes = False 
+variances = [1.0, 1.0, 1.0, 1.0] 
+coords = 'centroids' 
+normalize_coords = False
+
+
+
+K.clear_session()  #to clear any prevoius models
+
+model = build_model(image_size=(img_height, img_width, img_channels),          # to create a model using build_model function with parameters passed
+                    n_classes=n_classes,
+                    l2_regularization=0.0,
+                    scales=scales,
+                    aspect_ratios_global=aspect_ratios,
+                    aspect_ratios_per_layer=None,
+                    two_boxes_for_ar1=two_boxes_for_ar1,
+                    steps=steps,
+                    offsets=offsets,
+                    limit_boxes=limit_boxes,
+                    variances=variances,
+                    coords=coords,
+                    normalize_coords=normalize_coords,
+                    subtract_mean=subtract_mean,
+                    divide_by_stddev=divide_by_stddev,
+                    swap_channels=False)
+
+
+
+adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=5e-04)      # optimiser values
+
+ssd_loss = SSDLoss(neg_pos_ratio=3, n_neg_min=0, alpha=1.0)                      #loss function
+
+model.compile(optimizer=adam, loss=ssd_loss.compute_loss)                       #create model
+
+"""# TODO: Set the path to the `.h5` file of the model to be loaded.
+model_path = '/home/quest/ssd_keras/ssd7.h5'
+
+# We need to create an SSDLoss object in order to pass that to the model loader.
+ssd_loss = SSDLoss(neg_pos_ratio=3, n_neg_min=0, alpha=1.0)
+
+K.clear_session() # Clear previous models from memory.
+
+model = load_model(model_path, custom_objects={'AnchorBoxes': AnchorBoxes,
+                                               'compute_loss': ssd_loss.compute_loss})"""
+
+
+train_dataset = BatchGenerator(box_output_format=['class_id', 'xmin', 'ymin', 'xmax', 'ymax'])
+val_dataset = BatchGenerator(box_output_format=['class_id', 'xmin', 'ymin', 'xmax', 'ymax'])
+
+
+train_images_dir      = '/home/quest/udacity_driving_datasets'
+train_labels_filename = '/home/quest/udacity_driving_datasets/labels_train.csv'
+
+
+val_images_dir      = '/home/quest/udacity_driving_datasets'
+val_labels_filename = '/home/quest/udacity_driving_datasets/labels_val.csv'
+
+train_dataset.parse_csv(images_dir=train_images_dir,
+                        labels_filename=train_labels_filename,
+                        input_format=['image_name', 'xmin', 'xmax', 'ymin', 'ymax', 'class_id'], 
+                        include_classes='all')
+
+val_dataset.parse_csv(images_dir=val_images_dir,
+                      labels_filename=val_labels_filename,
+                      input_format=['image_name', 'xmin', 'xmax', 'ymin', 'ymax', 'class_id'],
+                      include_classes='all')
+
+predictor_sizes = [model.get_layer('classes4').output_shape[1:3],
+                   model.get_layer('classes5').output_shape[1:3],
+                   model.get_layer('classes6').output_shape[1:3],
+                   model.get_layer('classes7').output_shape[1:3]]
+
+ssd_box_encoder = SSDBoxEncoder(img_height=img_height,
+                                img_width=img_width,
+                                n_classes=n_classes, 
+                                predictor_sizes=predictor_sizes,
+                                scales=scales,
+                                aspect_ratios_global=aspect_ratios,
+                                aspect_ratios_per_layer=None,
+                                two_boxes_for_ar1=two_boxes_for_ar1,
+                                steps=steps,
+                                offsets=offsets,
+                                limit_boxes=limit_boxes,
+                                variances=variances,
+                                pos_iou_threshold=0.5,
+                                neg_iou_threshold=0.2,
+                                coords=coords,
+                                normalize_coords=normalize_coords)
+
+
+
+batch_size = 16
+
+
+
+train_generator = train_dataset.generate(batch_size=batch_size,
+                                         shuffle=True,
+                                         train=True,
+                                         ssd_box_encoder=ssd_box_encoder,
+                                         equalize=False,
+                                         brightness=(0.5, 2, 0.5), 
+                                         flip=0.5, 
+                                         translate=((5, 50), (3, 30), 0.5), 
+                                         scale=(0.75, 1.3, 0.5), 
+                                         max_crop_and_resize=False,
+                                         random_pad_and_resize=False,
+                                         random_crop=False,
+                                         crop=False,
+                                         resize=False,
+                                         gray=False,
+                                         limit_boxes=True,
+                                         include_thresh=0.4)
+
+val_generator = val_dataset.generate(batch_size=batch_size,
+                                     shuffle=True,
+                                     train=True,
+                                     ssd_box_encoder=ssd_box_encoder,
+                                     equalize=False,
+                                     brightness=False,
+                                     flip=False,
+                                     translate=False,
+                                     scale=False,
+                                     max_crop_and_resize=False,
+                                     random_pad_and_resize=False,
+                                     random_crop=False,
+                                     crop=False,
+                                     resize=False,
+                                     gray=False,
+                                     limit_boxes=True,
+                                     include_thresh=0.4)
+
+
+n_train_samples = train_dataset.get_n_samples()
+n_val_samples = val_dataset.get_n_samples()
+
+
+epochs = 20
+
+history = model.fit_generator(generator = train_generator,
+                              steps_per_epoch = ceil(n_train_samples/batch_size),
+                              epochs = epochs,
+                              callbacks = [ModelCheckpoint('ssd7_weights_epoch-{epoch:02d}_loss-{loss:.4f}.h5',
+                                                           monitor='val_loss',
+                                                           verbose=1,
+                                                           save_best_only=True,
+                                                           save_weights_only=True,
+                                                           mode='auto',
+                                                           period=1),
+                                           EarlyStopping(monitor='val_loss',
+                                                         min_delta=0.0001,
+                                                         patience=10),
+                                           ReduceLROnPlateau(monitor='val_loss',
+                                                             factor=0.5,
+                                                             patience=0,
+                                                             epsilon=0.001,
+                                                             cooldown=0)],
+                              validation_data = val_generator,
+                              validation_steps = ceil(n_val_samples/batch_size))
+
+# TODO: Set the filename (without the .h5 file extension!) under which to save the model and weights.
+#       Do the same in the `ModelCheckpoint` callback above.
+model_name = 'ssd7'
+model.save('{}.h5'.format(model_name))
+model.save_weights('{}_weights.h5'.format(model_name))
+
+print()
+print("Model saved under {}.h5".format(model_name))
+print("Weights also saved separately under {}_weights.h5".format(model_name))
+print()
+
+
+predict_generator = val_dataset.generate(batch_size=1,
+                                         shuffle=True,
+                                         train=False,
+                                         returns={'processed_labels',
+                                                  'filenames'},
+                                         equalize=False,
+                                         brightness=False,
+                                         flip=False,
+                                         translate=False,
+                                         scale=False,
+                                         max_crop_and_resize=False,
+                                         random_pad_and_resize=False,
+                                         random_crop=False,
+                                         crop=False,
+                                         resize=False,
+                                         gray=False,
+                                         limit_boxes=True,
+                                         include_thresh=0.4)
+
+X, y_true, filenames = next(predict_generator)
+
+i = 0 # Which batch item to look at
+
+print("Image:", filenames[i])
+print()
+print("Ground truth boxes:\n")
+print(y_true[i])
+
+y_pred = model.predict(X)
+
+y_pred_decoded = decode_y2(y_pred,
+                           confidence_thresh=0.5,
+                           iou_threshold=0.4,
+                           top_k='all',
+                           input_coords='centroids',
+                           normalize_coords=False,
+                           img_height=None,
+                           img_width=None)
+
+np.set_printoptions(precision=2, suppress=True, linewidth=90)
+print("Decoded predictions (output format is [class_id, confidence, xmin, ymin, xmax, ymax]):\n")
+print(y_pred_decoded[i])
+
+
+# 5: Draw the predicted boxes onto the image
+
+plt.figure(figsize=(20,12))
+plt.imshow(X[i])
+
+current_axis = plt.gca()
+
+colors = plt.cm.hsv(np.linspace(0, 1, n_classes+1)).tolist()
+classes = ['background', 'car', 'truck', 'pedestrian', 'bicyclist', 'light'] # Just so we can print class names onto the image instead of IDs
+
+# Draw the ground truth boxes in green (omit the label for more clarity)
+for box in y_true[i]:
+    xmin = box[1]
+    ymin = box[2]
+    xmax = box[3]
+    ymax = box[4]
+    label = '{}'.format(classes[int(box[0])])
+    current_axis.add_patch(plt.Rectangle((xmin, ymin), xmax-xmin, ymax-ymin, color='green', fill=False, linewidth=2))  
+    #current_axis.text(xmin, ymin, label, size='x-large', color='white', bbox={'facecolor':'green', 'alpha':1.0})
+
+# Draw the predicted boxes in blue
+for box in y_pred_decoded[i]:
+    xmin = box[-4]
+    ymin = box[-3]
+    xmax = box[-2]
+    ymax = box[-1]
+    color = colors[int(box[0])]
+    label = '{}: {:.2f}'.format(classes[int(box[0])], box[1])
+    current_axis.add_patch(plt.Rectangle((xmin, ymin), xmax-xmin, ymax-ymin, color='blue', fill=False, linewidth=2))  
+    current_axis.text(xmin, ymin, label, size='x-large', color='white', bbox={'facecolor':'blue', 'alpha':1.0})
+
+print("its ok")
+plt.show()
